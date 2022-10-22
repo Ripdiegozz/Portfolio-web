e6ae42a70b6b26624c8b80b89dcfe89e4ad9162f
Diego Garcia <diegogarciag63@gmail.com>
2022-10-21 19:10:28 -0500

    Finished proyecy

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,15 @@
 // variable declarations
 let menu = document.getElementById('menu__box');
 let navMenu_btn = document.getElementById('menu-btn');
-let dark_btn = document.getElementById('switch')
-let sun__icon = document.getElementById('sun')
-let top_btn = document.getElementById('scrollTop')
+let dark_btn = document.getElementById('switch');
+let sun__icon = document.getElementById('sun');
+let top_btn = document.getElementById('scrollTop');
+let contact_btn = document.getElementById('contact-btn');
+let about_btn = document.getElementById('about-btn')
+let skills_btn = document.getElementById('skills-btn')
+let studies_btn = document.getElementById('studies-btn')
+let proyects_btn = document.getElementById('proyects-btn')
+
 // mobile-btn
 
 navMenu_btn.addEventListener('click', ()=>{
@@ -36,4 +42,58 @@ function scrollToTopFunction(pxScreen) {
     })
 }
 
-scrollToTopFunction(400);
+scrollToTopFunction(370);
+
+// nav menu buttons
+
+/*let contact_btn = document.getElementById('contact-btn');
+let about_btn = document.getElementById('about-btn')
+let skills_btn = document.getElementById('skills-btn')
+let studies_btn = document.getElementById('studies-btn')
+let proyects_btn = document.getElementById('proyects-btn')*/ 
+
+contact_btn.addEventListener('click', ()=>{
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    menu.classList.toggle('active');
+})
+about_btn.addEventListener('click', ()=>{
+    window.scroll({
+        top: 670,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    menu.classList.toggle('active');
+})
+skills_btn.addEventListener('click', ()=>{
+    window.scroll({
+        top: 1650,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    menu.classList.toggle('active');
+})
+studies_btn.addEventListener('click', ()=>{
+    window.scroll({
+        top: 400,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    menu.classList.toggle('active');
+})
+proyects_btn.addEventListener('click', ()=>{
+    window.scroll({
+        top: 400,
+        left: 0,
+        behavior: "smooth"
+    });
+
+    menu.classList.toggle('active');
+})
