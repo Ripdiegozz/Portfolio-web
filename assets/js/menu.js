37e414029f3103c462903c4564e37c41a6bd6b9e
Diego Garcia <diegogarciag63@gmail.com>
2022-10-24 18:46:32 -0500

    improved links

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,11 +4,6 @@ let navMenu_btn = document.getElementById('menu-btn');
 let dark_btn = document.getElementById('switch');
 let sun__icon = document.getElementById('sun');
 let top_btn = document.getElementById('scrollTop');
-let contact_btn = document.getElementById('contact-btn');
-let about_btn = document.getElementById('about-btn')
-let skills_btn = document.getElementById('skills-btn')
-let studies_btn = document.getElementById('studies-btn')
-let proyects_btn = document.getElementById('proyects-btn')
 
 // mobile-btn
 
@@ -43,65 +38,3 @@ function scrollToTopFunction(pxScreen) {
 }
 
 scrollToTopFunction(370);
-
-// nav menu buttons
-
-/*let contact_btn = document.getElementById('contact-btn');
-let about_btn = document.getElementById('about-btn')
-let skills_btn = document.getElementById('skills-btn')
-let studies_btn = document.getElementById('studies-btn')
-let proyects_btn = document.getElementById('proyects-btn')*/ 
-
-contact_btn.addEventListener('click', ()=>{
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-    
-    menu.classList.toggle('active');
-
-});
-
-about_btn.addEventListener('click', ()=>{
-    window.scroll({
-        top: 670,
-        left: 0,
-        behavior: "smooth"
-    });
-    
-    menu.classList.toggle('active');
-
-});
-
-skills_btn.addEventListener('click', ()=>{
-    window.scroll({
-        top: 1650,
-        left: 0,
-        behavior: "smooth"
-    });
-
-    menu.classList.toggle('active');
-
-});
-studies_btn.addEventListener('click', ()=>{
-    window.scroll({
-        top: 2150,
-        left: 0,
-        behavior: "smooth"
-    });
-
-    menu.classList.toggle('active');
-
-});
-
-proyects_btn.addEventListener('click', ()=>{
-    window.scroll({
-        top: 3150,
-        left: 0,
-        behavior: "smooth"
-    });
-    
-    menu.classList.toggle('active');
-
-});
